205bd8fdb3e365f97ab67b7cd3dc0686e7fc19ef
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-09-28 12:07:07 +0330

    add interfaces

---
+++ src/types/exercise.ts
@@ -0,0 +1,25 @@
+export interface Exercise {
+  id: string;
+  name: string;
+  slug: string;
+  description: string;
+  category: string;
+
+  body_part: string;
+  //   muscleGroup: string;
+  difficulty: 'beginner' | 'intermediate' | 'advanced';
+  exercise_type: string;
+  //   equipment?: string;
+  instructions?: string;
+  video_link?: string;
+  image?: string;
+  //   createdAt: string;
+  //   updatedAt: string;
+}
+
+export interface ExerciseListResponse {
+  exercises: Exercise[];
+  total: number;
+  page: number;
+  limit: number;
+}
