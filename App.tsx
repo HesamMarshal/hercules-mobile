68d7cb94662b02f4a8ac201092cc2947e43076a1
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-10-01 12:06:48 +0330

    add RTLProvider

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from './src/contexts/AuthContext';
 import { AppNavigator } from './src/navigation/AppNavigator';
 
 // Import required for react-native-screens (improves performance)
 import { enableScreens } from 'react-native-screens';
+import { RTLProvider } from '@/contexts/RTLContext';
 enableScreens();
 
 export default function App() {
   return (
-    <AuthProvider>
-      <StatusBar style="auto" />
-      <AppNavigator />
-    </AuthProvider>
+    // TODO: Check ERROR BOUNDARY
+  //  <ErrorBoundary>
+      <RTLProvider>
+        <AuthProvider>
+          <AppNavigator />
+        </AuthProvider>
+      </RTLProvider>
+    // </ErrorBoundary>
   );
 }
