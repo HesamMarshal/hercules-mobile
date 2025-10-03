4d43a6ab66b7294ced1b332434d302d5206e9dfb
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-10-03 10:49:51 +0330

    add exercises to workouts interface

--- src/types/workout.type.ts
+++ src/types/workout.type.ts
@@ -1,8 +1,12 @@
+import { Exercise } from './exercise';
+
 export interface Workout {
   id: string;
   name: string;
   order: number;
   day_of_week: string; //enum
+  // TODO: replace with practice
+  exercises?: Exercise[];
 }
 
 export interface ExerciseListResponse {
