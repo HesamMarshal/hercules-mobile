93cd37f08699fcf13dc430eabb22474d47633258
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-10-03 20:01:22 +0330

    add PaperProvider

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
 // App.tsx
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { I18nextProvider } from 'react-i18next';
 import { StatusBar } from 'react-native';
 import { AppNavigator } from './src/navigation/AppNavigator';
-
 import { enableScreens } from 'react-native-screens';
 import { RTLProvider } from '@/contexts/RTLContext';
 import { AuthProvider } from '@/contexts/AuthContext';
+import { PaperProvider } from 'react-native-paper';
 
 enableScreens();
 
@@ -15,20 +13,13 @@ export default function App() {
   return (
     // TODO: Check ERROR BOUNDARY
     // <ErrorBoundary>
-    <RTLProvider>
-      <AuthProvider>
-        <AppNavigator />
-      </AuthProvider>
-    </RTLProvider>
+    <PaperProvider>
+      <RTLProvider>
+        <AuthProvider>
+          <AppNavigator />
+        </AuthProvider>
+      </RTLProvider>
+    </PaperProvider>
     // </ErrorBoundary>
-
-    // <I18nextProvider i18n={i18n}>
-    //   <AuthProvider>
-    //     <NavigationContainer>
-    //       <StatusBar barStyle="dark-content" />
-    //       <AppNavigator />
-    //     </NavigationContainer>
-    //   </AuthProvider>
-    // </I18nextProvider>
   );
 }
