37f6cdc9ea122b2d1dcb466a263611b6edea3309
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2025-10-05 14:17:57 +0330

    use Exercise Interface

--- src/interfaces/practice.interface.ts
+++ src/interfaces/practice.interface.ts
@@ -1,20 +1,13 @@
+import { Exercise } from './exercise.interface';
+
 export interface Practice {
   id: string;
-  exercise_id: string;
   workout_id: string;
+  exercise_id: string;
   sets: number;
   reps: number;
   weight: number;
   rest_time: number;
   order: number;
-  exercise: {
-    id: string;
-    name: string;
-    description: string;
-    video_url: string;
-    primary_muscle_group: string;
-    secondary_muscle_group: string;
-    equipment: string;
-    difficulty: string;
-  };
+  exercise?: Exercise;
 }
